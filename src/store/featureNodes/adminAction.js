b03ef1f918710d6cb7d467e135fb4f7ad58a1b15
Caprice123 <kaguya260401@gmail.com>
2026-08-06 18:13:56 +0700

    fix upload

--- src/store/featureNodes/adminAction.js
+++ src/store/featureNodes/adminAction.js
@@ -167,7 +167,7 @@ export const swapNodeOrder = (nodeId, withNodeId, onSuccess) => async (dispatch)
   }
 }
 
-export const uploadNodeVideo = (file, provider = 'idrive') => async (dispatch) => {
+export const uploadNodeVideo = (file, provider = 'bunny_stream') => async (dispatch) => {
   try {
     dispatch(setLoading({ isUploadingVideo: true }))
     const formData = new FormData()
@@ -180,4 +180,3 @@ export const uploadNodeVideo = (file, provider = 'idrive') => async (dispatch) =
     dispatch(setLoading({ isUploadingVideo: false }))
   }
 }
-
